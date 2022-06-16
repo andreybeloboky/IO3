658937e40259ad00d20cd9a92f5caa40a25e1655
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-06-17 01:17:25 +0300

    add new streams and copy from file1 bytes to file2 in the opposite direction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (FileOutputStream file = new FileOutputStream("test.txt");) {
+            String test = "i've checked your tests";
+            file.write(test.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (FileOutputStream file2 = new FileOutputStream("test2.txt");
+             FileInputStream file1 = new FileInputStream("test.txt");) {
+            int byteNumber;
+            ArrayList<Integer> array = new ArrayList<>();
+            while ((byteNumber = file1.read()) != -1) {
+                array.add(byteNumber);
+            }
+            for (int i = array.size()-1; i >= 0; i--) {
+                file2.write(array.get(i));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
